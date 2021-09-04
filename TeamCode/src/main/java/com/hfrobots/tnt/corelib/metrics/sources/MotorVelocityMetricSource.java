9ea2ef1f8bf983da46f9423f4d97f6479099da92
FTC-9929 <tnt9929@hfrobots.com>
2021-09-04 09:03:07 -0500

    Use built-ins for measuring velocity

--- TeamCode/src/main/java/com/hfrobots/tnt/corelib/metrics/sources/MotorVelocityMetricSource.java
+++ TeamCode/src/main/java/com/hfrobots/tnt/corelib/metrics/sources/MotorVelocityMetricSource.java
@@ -1,7 +1,25 @@
+/*
+ Copyright (c) 2021 HF Robotics (http://www.hfrobots.com)
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+*/
+
 package com.hfrobots.tnt.corelib.metrics.sources;
 
 import com.hfrobots.tnt.corelib.metrics.GaugeMetricSource;
-import com.hfrobots.tnt.corelib.metrics.MetricsSampler;
 import com.hfrobots.tnt.util.NamedDeviceMap;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
@@ -14,16 +32,10 @@ public class MotorVelocityMetricSource implements GaugeMetricSource {
 
     private final String sampleName;
 
-    private int lastEncoderValue;
-
-    private long lastValueTimestamp;
-
     public MotorVelocityMetricSource(@NonNull final NamedDeviceMap.NamedDevice<DcMotorEx> namedMotor) {
         this.motor = namedMotor.getDevice();
 
         sampleName = String.format("dcm_vel_%s", namedMotor.getName());
-        lastEncoderValue = motor.getCurrentPosition();
-        lastValueTimestamp = System.currentTimeMillis();
     }
 
     @Override
@@ -33,18 +45,6 @@ public class MotorVelocityMetricSource implements GaugeMetricSource {
 
     @Override
     public double getValue() {
-        int newEncoderValue = motor.getCurrentPosition();
-        double deltaEncoderPosition = newEncoderValue - lastEncoderValue;
-        lastEncoderValue = newEncoderValue;
-
-        long now = System.currentTimeMillis();
-        double deltaTimeMillis = now - lastValueTimestamp;
-        lastValueTimestamp = now;
-
-        if (deltaTimeMillis == 0) {
-            return MetricsSampler.NO_REPORT_VALUE;
-        }
-
-        return (deltaEncoderPosition / deltaTimeMillis) * 1000;
+        return motor.getVelocity();
     }
 }
