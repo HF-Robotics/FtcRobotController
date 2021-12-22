87d4108a2bbe1524ff32c65a0f6756eecd51a857
FTC-9929 <tnt9929@hfrobots.com>
2021-12-22 13:04:10 -0600

    Another pipeline test for harsh lighting

--- TeamCode/src/main/java/com/hfrobots/tnt/season2122/FindDuckContours.java
+++ TeamCode/src/main/java/com/hfrobots/tnt/season2122/FindDuckContours.java
@@ -6,8 +6,10 @@ import org.opencv.core.Mat;
 import org.opencv.core.MatOfInt;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
 import java.util.ArrayList;
@@ -21,15 +23,12 @@ import java.util.List;
  * @author GRIP
  */
 public class FindDuckContours {
-
     //Outputs
     private Mat hsvThresholdOutput = new Mat();
+    private Mat blurOutput = new Mat();
+    private Mat cvDilateOutput = new Mat();
     private ArrayList<MatOfPoint> findContoursOutput = new ArrayList<MatOfPoint>();
     private ArrayList<MatOfPoint> filterContoursOutput = new ArrayList<MatOfPoint>();
-
-    static {
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-    }
 
     /**
      * This is the primary method that runs the entire pipeline and updates the outputs.
@@ -37,29 +36,44 @@ public class FindDuckContours {
     public void process(Mat source0) {
         // Step HSV_Threshold0:
         Mat hsvThresholdInput = source0;
-        double[] hsvThresholdHue = {24.280575539568343, 33.310696095076395};
-        double[] hsvThresholdSaturation = {59.62230215827338, 255.0};
-        double[] hsvThresholdValue = {22.93165467625899, 255.0};
+        double[] hsvThresholdHue = {12.949640287769784, 48.59083191850594};
+        double[] hsvThresholdSaturation = {64.20863309352518, 255.0};
+        double[] hsvThresholdValue = {137.58992805755395, 255.0};
         hsvThreshold(hsvThresholdInput, hsvThresholdHue, hsvThresholdSaturation, hsvThresholdValue, hsvThresholdOutput);
 
+        // Step Blur0:
+        Mat blurInput = hsvThresholdOutput;
+        BlurType blurType = BlurType.get("Gaussian Blur");
+        double blurRadius = 5.405405405405405;
+        blur(blurInput, blurType, blurRadius, blurOutput);
+
+        // Step CV_dilate0:
+        Mat cvDilateSrc = blurOutput;
+        Mat cvDilateKernel = new Mat();
+        Point cvDilateAnchor = new Point(-1, -1);
+        double cvDilateIterations = 5.0;
+        int cvDilateBordertype = Core.BORDER_CONSTANT;
+        Scalar cvDilateBordervalue = new Scalar(-1);
+        cvDilate(cvDilateSrc, cvDilateKernel, cvDilateAnchor, cvDilateIterations, cvDilateBordertype, cvDilateBordervalue, cvDilateOutput);
+
         // Step Find_Contours0:
-        Mat findContoursInput = hsvThresholdOutput;
+        Mat findContoursInput = cvDilateOutput;
         boolean findContoursExternalOnly = false;
         findContours(findContoursInput, findContoursExternalOnly, findContoursOutput);
 
         // Step Filter_Contours0:
         ArrayList<MatOfPoint> filterContoursContours = findContoursOutput;
-        double filterContoursMinArea = 0;
-        double filterContoursMinPerimeter = 0;
-        double filterContoursMinWidth = 0;
-        double filterContoursMaxWidth = 1000;
-        double filterContoursMinHeight = 10.0;
-        double filterContoursMaxHeight = 1000;
+        double filterContoursMinArea = 0.0;
+        double filterContoursMinPerimeter = 0.0;
+        double filterContoursMinWidth = 0.0;
+        double filterContoursMaxWidth = 1000.0;
+        double filterContoursMinHeight = 40.0;
+        double filterContoursMaxHeight = 1000.0;
         double[] filterContoursSolidity = {0, 100};
-        double filterContoursMaxVertices = 1000000;
-        double filterContoursMinVertices = 0;
-        double filterContoursMinRatio = 0;
-        double filterContoursMaxRatio = 1000;
+        double filterContoursMaxVertices = 1000000.0;
+        double filterContoursMinVertices = 0.0;
+        double filterContoursMinRatio = 0.0;
+        double filterContoursMaxRatio = 1000.0;
         filterContours(filterContoursContours, filterContoursMinArea, filterContoursMinPerimeter, filterContoursMinWidth, filterContoursMaxWidth, filterContoursMinHeight, filterContoursMaxHeight, filterContoursSolidity, filterContoursMaxVertices, filterContoursMinVertices, filterContoursMinRatio, filterContoursMaxRatio, filterContoursOutput);
 
     }
@@ -70,6 +84,22 @@ public class FindDuckContours {
      */
     public Mat hsvThresholdOutput() {
         return hsvThresholdOutput;
+    }
+
+    /**
+     * This method is a generated getter for the output of a Blur.
+     * @return Mat output from Blur.
+     */
+    public Mat blurOutput() {
+        return blurOutput;
+    }
+
+    /**
+     * This method is a generated getter for the output of a CV_dilate.
+     * @return Mat output from CV_dilate.
+     */
+    public Mat cvDilateOutput() {
+        return cvDilateOutput;
     }
 
     /**
@@ -102,6 +132,95 @@ public class FindDuckContours {
         Imgproc.cvtColor(input, out, Imgproc.COLOR_BGR2HSV);
         Core.inRange(out, new Scalar(hue[0], sat[0], val[0]),
                 new Scalar(hue[1], sat[1], val[1]), out);
+    }
+
+    /**
+     * An indication of which type of filter to use for a blur.
+     * Choices are BOX, GAUSSIAN, MEDIAN, and BILATERAL
+     */
+    enum BlurType{
+        BOX("Box Blur"), GAUSSIAN("Gaussian Blur"), MEDIAN("Median Filter"),
+        BILATERAL("Bilateral Filter");
+
+        private final String label;
+
+        BlurType(String label) {
+            this.label = label;
+        }
+
+        public static BlurType get(String type) {
+            if (BILATERAL.label.equals(type)) {
+                return BILATERAL;
+            }
+            else if (GAUSSIAN.label.equals(type)) {
+                return GAUSSIAN;
+            }
+            else if (MEDIAN.label.equals(type)) {
+                return MEDIAN;
+            }
+            else {
+                return BOX;
+            }
+        }
+
+        @Override
+        public String toString() {
+            return this.label;
+        }
+    }
+
+    /**
+     * Softens an image using one of several filters.
+     * @param input The image on which to perform the blur.
+     * @param type The blurType to perform.
+     * @param doubleRadius The radius for the blur.
+     * @param output The image in which to store the output.
+     */
+    private void blur(Mat input, BlurType type, double doubleRadius,
+                      Mat output) {
+        int radius = (int)(doubleRadius + 0.5);
+        int kernelSize;
+        switch(type){
+            case BOX:
+                kernelSize = 2 * radius + 1;
+                Imgproc.blur(input, output, new Size(kernelSize, kernelSize));
+                break;
+            case GAUSSIAN:
+                kernelSize = 6 * radius + 1;
+                Imgproc.GaussianBlur(input,output, new Size(kernelSize, kernelSize), radius);
+                break;
+            case MEDIAN:
+                kernelSize = 2 * radius + 1;
+                Imgproc.medianBlur(input, output, kernelSize);
+                break;
+            case BILATERAL:
+                Imgproc.bilateralFilter(input, output, -1, radius, radius);
+                break;
+        }
+    }
+
+    /**
+     * Expands area of higher value in an image.
+     * @param src the Image to dilate.
+     * @param kernel the kernel for dilation.
+     * @param anchor the center of the kernel.
+     * @param iterations the number of times to perform the dilation.
+     * @param borderType pixel extrapolation method.
+     * @param borderValue value to be used for a constant border.
+     * @param dst Output Image.
+     */
+    private void cvDilate(Mat src, Mat kernel, Point anchor, double iterations,
+                          int borderType, Scalar borderValue, Mat dst) {
+        if (kernel == null) {
+            kernel = new Mat();
+        }
+        if (anchor == null) {
+            anchor = new Point(-1,-1);
+        }
+        if (borderValue == null){
+            borderValue = new Scalar(-1);
+        }
+        Imgproc.dilate(src, dst, kernel, anchor, (int)iterations, borderType, borderValue);
     }
 
     /**
