8e5c0f3c3a85e08acc0ee277641b6feac92ce90b
Mark Matthews <markdmatthews@users.noreply.github.com>
2022-01-12 10:05:55 -0600

    Added barcode test tele-op to speed up tuning of the pipeline and detection logic

---
+++ TeamCode/src/main/java/com/hfrobots/tnt/season2122/BarcodePipelineTester.java
@@ -0,0 +1,15 @@
+package com.hfrobots.tnt.season2122;
+
+import com.hfrobots.tnt.corelib.vision.OpenCvTestTeleop;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.openftc.easyopencv.OpenCvPipeline;
+
+@SuppressWarnings("unused")
+@TeleOp(name="00 Barcode Tester", group="Utilities")
+public class BarcodePipelineTester extends OpenCvTestTeleop {
+    @Override
+    protected OpenCvPipeline getPipeline() {
+        return new BarcodeDetectorPipeline(telemetry, true);
+    }
+}
