fb5efd02ab112544cc0f80427e2799720674f0dd
FTC-9929 Shared Laptops <tnt9929@hfrobots.com>
2022-12-23 17:22:10 -0600

    Removed deprecated or no-longer supported sensors

--- TeamCode/src/main/java/com/hfrobots/tnt/util/SensorTester.java
+++ TeamCode/src/main/java/com/hfrobots/tnt/util/SensorTester.java
@@ -24,16 +24,12 @@ import com.ftc9929.corelib.control.NinjaGamePad;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.lynx.LynxEmbeddedIMU;
 import com.qualcomm.hardware.lynx.LynxI2cColorRangeSensor;
-import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cColorSensor;
-import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
 import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
+import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.AnalogInput;
-import com.qualcomm.robotcore.hardware.CompassSensor;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
-import com.qualcomm.robotcore.hardware.GyroSensor;
-import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
 import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
@@ -56,21 +52,13 @@ import java.util.List;
 @TeleOp(name="Sensor Tester", group="Utilities")
 @SuppressWarnings("unused")
 public class SensorTester extends OpMode {
-    private List<NamedDeviceMap.NamedDevice<ModernRoboticsI2cColorSensor>> namedColorSensors;
-
     private List<NamedDeviceMap.NamedDevice<Rev2mDistanceSensor>> namedTofSensors;
-
-    private List<NamedDeviceMap.NamedDevice<ModernRoboticsI2cRangeSensor>> namedRangeSensors;
-
-    private List<NamedDeviceMap.NamedDevice<OpticalDistanceSensor>> namedOdsSensors;
-
-    private List<NamedDeviceMap.NamedDevice<GyroSensor>> namedGyroDevices;
-
-    private List<NamedDeviceMap.NamedDevice<CompassSensor>> namedCompassDevices;
 
     private List<NamedDeviceMap.NamedDevice<TouchSensor>> namedTouchDevices;
 
     private List<NamedDeviceMap.NamedDevice<LynxI2cColorRangeSensor>> namedLynxColorSensors;
+
+    private List<NamedDeviceMap.NamedDevice<RevColorSensorV3>> namedRevV3ColorSensors;
 
     private List<NamedDeviceMap.NamedDevice<DigitalChannel>> namedDigitalChannels;
 
@@ -83,15 +71,7 @@ public class SensorTester extends OpMode {
     private DebouncedButton leftBumper;
     private DebouncedButton rightBumper;
 
-    private DebouncedButton dpadUp;
-
-    private DebouncedButton dpadDown;
-
     private DebouncedButton aButton;
-
-    private DebouncedButton xButton;
-
-    private DebouncedButton yButton;
 
     private NamedDeviceMap namedDeviceMap;
 
@@ -101,32 +81,18 @@ public class SensorTester extends OpMode {
 
         namedDigitalChannels = namedDeviceMap.getAll(DigitalChannel.class);
         namedLynxColorSensors = namedDeviceMap.getAll(LynxI2cColorRangeSensor.class);
-        namedColorSensors = namedDeviceMap.getAll(ModernRoboticsI2cColorSensor.class);
-        namedRangeSensors = namedDeviceMap.getAll(ModernRoboticsI2cRangeSensor.class);
-        namedOdsSensors = namedDeviceMap.getAll(OpticalDistanceSensor.class);
-        namedGyroDevices = namedDeviceMap.getAll(GyroSensor.class);
-        namedCompassDevices = namedDeviceMap.getAll(CompassSensor.class);
+        namedRevV3ColorSensors = namedDeviceMap.getAll(RevColorSensorV3.class);
         namedTouchDevices = namedDeviceMap.getAll(TouchSensor.class);
         namedImus = namedDeviceMap.getAll(LynxEmbeddedIMU.class);
         namedTofSensors = namedDeviceMap.getAll(Rev2mDistanceSensor.class);
         namedAnalogInputs = namedDeviceMap.getAll(AnalogInput.class);
 
-        for (NamedDeviceMap.NamedDevice<GyroSensor> namedGyro : namedGyroDevices) {
-            namedGyro.getDevice().calibrate();
-        }
-
         initializeLynxEmbeddedImus();
-
-        currentListPosition = 0;
 
         NinjaGamePad ninjaGamePad = new NinjaGamePad(gamepad1);
         leftBumper = new DebouncedButton(ninjaGamePad.getLeftBumper());
         rightBumper = new DebouncedButton(ninjaGamePad.getRightBumper());
-        dpadUp = new DebouncedButton(ninjaGamePad.getDpadUp());
-        dpadDown = new DebouncedButton(ninjaGamePad.getDpadDown());
         aButton = new DebouncedButton(ninjaGamePad.getAButton());
-        xButton = new DebouncedButton(ninjaGamePad.getXButton());
-        yButton = new DebouncedButton(ninjaGamePad.getYButton());
     }
 
     protected void initializeLynxEmbeddedImus() {
@@ -147,53 +113,39 @@ public class SensorTester extends OpMode {
 
     private boolean ledEnabled = false;
 
-    private enum Mode { COLOR, LYNX_COLOR, LYNX_IMU, TOF_SENSOR, RANGE, ODS, DIGITAL_CHANNEL, ANALOG_INPUT, GYRO, COMPASS, TOUCH }
+    private enum Mode { LYNX_COLOR, REV_COLOR_V3, LYNX_IMU, TOF_SENSOR,  DIGITAL_CHANNEL, ANALOG_INPUT, TOUCH }
 
-    private Mode currentMode = Mode.COLOR;
+    private Mode currentMode = Mode.LYNX_COLOR;
 
     @Override
     public void loop() {
         if (leftBumper.getRise()) {
             switch (currentMode) {
-                case COLOR:
-                    currentMode = Mode.LYNX_COLOR;
-                    break;
                 case LYNX_COLOR:
+                    currentMode = Mode.REV_COLOR_V3;
+                    break;
+                case REV_COLOR_V3:
                     currentMode = Mode.LYNX_IMU;
                     break;
                 case LYNX_IMU:
                     currentMode = Mode.TOF_SENSOR;
                     break;
                 case TOF_SENSOR:
-                    currentMode = Mode.RANGE;
-                case RANGE:
-                    currentMode = Mode.ODS;
-                    break;
-                case ODS:
                     currentMode = Mode.DIGITAL_CHANNEL;
                     break;
                 case DIGITAL_CHANNEL:
                     currentMode = Mode.ANALOG_INPUT;
                     break;
                 case ANALOG_INPUT:
-                    currentMode = Mode.GYRO;
-                    break;
-                case GYRO:
-                    currentMode = Mode.COMPASS;
-                    break;
-                case COMPASS:
                     currentMode = Mode.TOUCH;
                     break;
                 case TOUCH:
-                    currentMode = Mode.COLOR;
+                    currentMode = Mode.LYNX_COLOR;
                     break;
             }
         }
 
         switch (currentMode) {
-            case COLOR:
-                doColorSensorLoop();
-                break;
             case LYNX_COLOR:
                 doLynxColorSensorLoop();
                 break;
@@ -203,57 +155,16 @@ public class SensorTester extends OpMode {
             case TOF_SENSOR:
                 doTofSensorLoop();
                 break;
-            case RANGE:
-                doRangeSensorLoop();
-                break;
-            case ODS:
-                doOdsSensorLoop();
-                break;
             case DIGITAL_CHANNEL:
                 doDigitalChannelLoop();
                 break;
             case ANALOG_INPUT:
                 doAnalogInputLoop();
                 break;
-            case GYRO:
-                doGyroSensorLoop();
-                break;
-            case COMPASS:
-                doCompassSensorLoop();
-                break;
             case TOUCH:
                 doTouchSensorLoop();
                 break;
         }
-    }
-
-    private int currentOpticalDistanceSensorListPosition = 0;
-
-    private void doOdsSensorLoop() {
-        namedOdsSensors = namedDeviceMap.getAll(OpticalDistanceSensor.class);
-
-
-        if (namedOdsSensors.isEmpty()) {
-            telemetry.addData("No optical distance sensors", "");
-            updateTelemetry(telemetry);
-            return;
-        }
-
-        if (rightBumper.getRise()) {
-            currentOpticalDistanceSensorListPosition++;
-
-            if (currentOpticalDistanceSensorListPosition == namedOdsSensors.size()) {
-                currentOpticalDistanceSensorListPosition = 0;
-            }
-        }
-
-        NamedDeviceMap.NamedDevice<OpticalDistanceSensor> currentNamedOdsSensor = namedOdsSensors.get(currentOpticalDistanceSensorListPosition);
-        OpticalDistanceSensor currentOdsSensor = currentNamedOdsSensor.getDevice();
-        String sensorName = currentNamedOdsSensor.getName();
-        telemetry.addData("ODS ",  "%s", sensorName);
-        telemetry.addData("Raw cm", "%f", currentOdsSensor.getRawLightDetected());
-        telemetry.addData("Calibrated cm", "%f", currentOdsSensor.getLightDetected());
-        updateTelemetry(telemetry);
     }
 
     private int currentTofSensorListPosition = 0;
@@ -285,73 +196,6 @@ public class SensorTester extends OpMode {
         updateTelemetry(telemetry);
     }
 
-    private int currentRangeSensorListPosition = 0;
-
-    private void doRangeSensorLoop() {
-        namedRangeSensors = namedDeviceMap.getAll(ModernRoboticsI2cRangeSensor.class);
-
-        if (namedRangeSensors.isEmpty()) {
-            telemetry.addData("No range sensors", "");
-            updateTelemetry(telemetry);
-            return;
-        }
-
-        if (rightBumper.getRise()) {
-            currentRangeSensorListPosition++;
-
-            if (currentRangeSensorListPosition == namedRangeSensors.size()) {
-                currentRangeSensorListPosition = 0;
-            }
-        }
-
-        NamedDeviceMap.NamedDevice<ModernRoboticsI2cRangeSensor> currentNamedRangeSensor = namedRangeSensors.get(currentRangeSensorListPosition);
-        ModernRoboticsI2cRangeSensor currentRangeSensor = currentNamedRangeSensor.getDevice();
-        String sensorName = currentNamedRangeSensor.getName();
-        telemetry.addData("range ",  "%s", sensorName);
-        telemetry.addData("Ultrasonic cm", "%f", currentRangeSensor.cmUltrasonic());
-        telemetry.addData("Optical cm", "%f", currentRangeSensor.cmOptical());
-        updateTelemetry(telemetry);
-    }
-
-    private void doColorSensorLoop() {
-        namedColorSensors = namedDeviceMap.getAll(ModernRoboticsI2cColorSensor.class);
-
-        if (namedColorSensors.isEmpty()) {
-            telemetry.addData("No color sensors", "");
-            updateTelemetry(telemetry);
-            return;
-        }
-
-        if (rightBumper.getRise()) {
-            currentListPosition++;
-
-            if (currentListPosition == namedColorSensors.size()) {
-                currentListPosition = 0;
-            }
-        }
-
-        NamedDeviceMap.NamedDevice<ModernRoboticsI2cColorSensor> currentNamedColorSensor = namedColorSensors.get(currentListPosition);
-        ModernRoboticsI2cColorSensor currentColorSensor = currentNamedColorSensor.getDevice();
-        String sensorName = currentNamedColorSensor.getName();
-
-        if (aButton.getRise()) {
-            ledEnabled = !ledEnabled;
-        }
-
-        currentColorSensor.enableLed(ledEnabled);
-        int alpha = currentColorSensor.alpha();
-        int red = currentColorSensor.red();
-        int green = currentColorSensor.green();
-        int blue = currentColorSensor.blue();
-
-        telemetry.addData("color ",  "%s x blk cal, y wt cal", sensorName);
-        telemetry.addData("R", "%d", red);
-        telemetry.addData("G", "%d", green);
-        telemetry.addData("B", "%d", blue);
-        telemetry.addData("A", "%d", alpha);
-        updateTelemetry(telemetry);
-    }
-
     int currentLynxColorListPosition = 0;
 
     private void doLynxColorSensorLoop() {
@@ -374,6 +218,45 @@ public class SensorTester extends OpMode {
         NamedDeviceMap.NamedDevice<LynxI2cColorRangeSensor> currentNamedLynxColorSensor = namedLynxColorSensors.get(currentLynxColorListPosition);
         LynxI2cColorRangeSensor currentColorSensor = currentNamedLynxColorSensor.getDevice();
         String sensorName = currentNamedLynxColorSensor.getName();
+
+        if (aButton.getRise()) {
+            ledEnabled = !ledEnabled;
+        }
+
+        currentColorSensor.enableLed(ledEnabled);
+        int alpha = currentColorSensor.alpha();
+        int red = currentColorSensor.red();
+        int green = currentColorSensor.green();
+        int blue = currentColorSensor.blue();
+
+        telemetry.addData("color ",  "%s x blk cal, y wt cal", sensorName);
+        telemetry.addData("R G B A", "%d %d %d %d", red, green, blue, alpha);
+        telemetry.addData("D (mm):", "%f", currentColorSensor.getDistance(DistanceUnit.MM));
+        telemetry.addData("light:", "%f", currentColorSensor.getLightDetected());
+        telemetry.addData("raw light:", "%f", currentColorSensor.getRawLightDetected());
+        updateTelemetry(telemetry);
+    }
+
+    int currentRevColorV3ListPosition = 0;
+
+    private void doRevColorSensorV3Loop() {
+        if (namedRevV3ColorSensors.isEmpty()) {
+            telemetry.addData("No REV V3 i2c color sensors", "");
+            updateTelemetry(telemetry);
+            return;
+        }
+
+        if (rightBumper.getRise()) {
+            currentRevColorV3ListPosition++;
+
+            if (currentRevColorV3ListPosition == namedRevV3ColorSensors.size()) {
+                currentRevColorV3ListPosition = 0;
+            }
+        }
+
+        NamedDeviceMap.NamedDevice<RevColorSensorV3> currentNamedRevColorSensorV3 = namedRevV3ColorSensors.get(currentRevColorV3ListPosition);
+        RevColorSensorV3 currentColorSensor = currentNamedRevColorSensorV3.getDevice();
+        String sensorName = currentNamedRevColorSensorV3.getName();
 
         if (aButton.getRise()) {
             ledEnabled = !ledEnabled;
@@ -499,68 +382,6 @@ public class SensorTester extends OpMode {
         updateTelemetry(telemetry);
     }
 
-    private int currentGyroListPosition = 0;
-
-    private void doGyroSensorLoop() {
-        if (namedGyroDevices.isEmpty()) {
-            telemetry.addData("No gyro sensors", "");
-            updateTelemetry(telemetry);
-            return;
-        }
-
-        if (rightBumper.getRise()) {
-            currentGyroListPosition++;
-
-            if (currentGyroListPosition == namedGyroDevices.size()) {
-                currentGyroListPosition = 0;
-            }
-        }
-
-        NamedDeviceMap.NamedDevice<GyroSensor> currentNamedGyroSensor = namedGyroDevices.get(currentGyroListPosition);
-        GyroSensor currentGyroSensor = currentNamedGyroSensor.getDevice();
-        String sensorName = currentNamedGyroSensor.getName();
-
-        int rawX = currentGyroSensor.rawX();
-        int rawY = currentGyroSensor.rawY();
-        int rawZ = currentGyroSensor.rawZ();
-        int heading = currentGyroSensor.getHeading();
-
-        telemetry.addData("gyro ",  "%s", sensorName);
-        telemetry.addData("X", "%d", rawX);
-        telemetry.addData("Y", "%d", rawY);
-        telemetry.addData("Z", "%d", rawZ);
-        telemetry.addData("H", "%d", heading);
-        updateTelemetry(telemetry);
-    }
-
-    private int currentCompassListPosition = 0;
-
-    private void doCompassSensorLoop() {
-        if (namedCompassDevices.isEmpty()) {
-            telemetry.addData("No compass sensors", "");
-            updateTelemetry(telemetry);
-            return;
-        }
-
-        if (rightBumper.getRise()) {
-            currentCompassListPosition++;
-
-            if (currentCompassListPosition == namedCompassDevices.size()) {
-                currentCompassListPosition = 0;
-            }
-        }
-
-        NamedDeviceMap.NamedDevice<CompassSensor> currentNamedCompassSensor = namedCompassDevices.get(currentCompassListPosition);
-        CompassSensor currentCompassSensor = currentNamedCompassSensor.getDevice();
-        currentCompassSensor.setMode(CompassSensor.CompassMode.MEASUREMENT_MODE);
-        String sensorName = currentNamedCompassSensor.getName();
-
-        double direction = currentCompassSensor.getDirection();
-        telemetry.addData("compass ",  "%s", sensorName);
-        telemetry.addData("direction", "%s", Double.toString(direction));
-        updateTelemetry(telemetry);
-    }
-
     private int currentTouchSensorListPosition = 0;
 
     private void doTouchSensorLoop() {
@@ -573,7 +394,7 @@ public class SensorTester extends OpMode {
         if (rightBumper.getRise()) {
             currentTouchSensorListPosition++;
 
-            if (currentTouchSensorListPosition == namedCompassDevices.size()) {
+            if (currentTouchSensorListPosition == namedTouchDevices.size()) {
                 currentTouchSensorListPosition = 0;
             }
         }
