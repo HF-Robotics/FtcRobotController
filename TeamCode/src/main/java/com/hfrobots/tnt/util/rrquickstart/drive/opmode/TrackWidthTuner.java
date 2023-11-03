56b73a06f09b07e05b7190ffc23684d82ef82a2a
FTC-9929 Shared Laptops <tnt9929@hfrobots.com>
2023-11-02 21:35:42 -0500

    Changes for rr quickstart for centerstage seasom

--- TeamCode/src/main/java/com/hfrobots/tnt/util/rrquickstart/drive/opmode/TrackWidthTuner.java
+++ TeamCode/src/main/java/com/hfrobots/tnt/util/rrquickstart/drive/opmode/TrackWidthTuner.java
@@ -5,6 +5,8 @@ import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.util.Angle;
+import com.hfrobots.tnt.corelib.drive.mecanum.RoadRunnerMecanumDriveBase;
+import com.hfrobots.tnt.season2324.CenterstageAuto;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -35,7 +37,7 @@ public class TrackWidthTuner extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
-        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        RoadRunnerMecanumDriveBase drive = new RoadRunnerMecanumDriveBase(hardwareMap, new CenterstageAuto.CenterstageDriveConstants());
         // TODO: if you haven't already, set the localizer to something that doesn't depend on
         // drive encoders for computing the heading
 
