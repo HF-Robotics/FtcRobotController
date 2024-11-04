2cb2c8599e4103976fed7ca31129e7e853cf9a90
TNT Shared Laptop <tnt-enb@hfrobots.com>
2024-11-03 19:46:35 -0600

    Trevor - use gripper instead of rolling intake for now

--- TeamCode/src/main/java/com/hfrobots/tnt/season2425/IntoTheDeepScoringMech.java
+++ TeamCode/src/main/java/com/hfrobots/tnt/season2425/IntoTheDeepScoringMech.java
@@ -1,8 +1,10 @@
 package com.hfrobots.tnt.season2425;
 
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
 
 public class IntoTheDeepScoringMech {
     Arm arm;
@@ -16,14 +18,21 @@ public class IntoTheDeepScoringMech {
 
         Servo elbowServo;
 
+        Servo intakeServo;
+
         final double ELBOW_STOWED_SERVO_POSITION = 0.0D;
 
         final double ELBOW_UNSTOWED_SERVO_POSITION = .98D;
+
+        final double ARM_ANTI_GRAVITY_FEED_FORWARD = 0;
 
         Arm(final HardwareMap hardwareMap) {
             shoulderMotor = hardwareMap.get(DcMotorEx.class, "shoulderMotor");
             elbowServo = hardwareMap.get(Servo.class, "elbowServo");
             elbowServo.setPosition(ELBOW_STOWED_SERVO_POSITION);
+
+            intakeServo = hardwareMap.get(Servo.class, "intakeServo");
+            outtakeSample();
         }
 
         public void shoulderUp(float power) {
@@ -35,15 +44,39 @@ public class IntoTheDeepScoringMech {
         }
 
         public void stopShoulder() {
-            shoulderMotor.setPower(0);
+            if (forearmIsExtended) {
+                shoulderUp(0.24F);
+            } else {
+                shoulderMotor.setPower(0);
+            }
         }
 
+        private boolean forearmIsExtended = false;
+
         public void forearmOut() {
-            elbowServo.setPosition(ELBOW_UNSTOWED_SERVO_POSITION);
+            forearmIsExtended = true;
+            double elbowPosition = elbowServo.getPosition();
+            elbowPosition += .035;
+            elbowPosition = Math.min(elbowPosition, ELBOW_UNSTOWED_SERVO_POSITION);
+
+            elbowServo.setPosition(elbowPosition);
         }
 
         public void forearmIn() {
+            forearmIsExtended = false;
             elbowServo.setPosition(ELBOW_STOWED_SERVO_POSITION);
+        }
+
+        public void intakeSample() {
+            intakeServo.setPosition(1);
+        }
+
+        public void outtakeSample() {
+            intakeServo.setPosition(0);
+        }
+
+        public void stopIntake() {
+            //intakeServo.setPower(0);
         }
     }
 }
