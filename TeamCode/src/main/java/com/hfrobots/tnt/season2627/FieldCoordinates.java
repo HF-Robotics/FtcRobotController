a25018850ed8971c263ab111343d933bade3736e
TNT Shared Laptop <tnt-enb@hfrobots.com>
2026-09-20 16:31:29 -0500

    Morgan - added field coordinates for re-use in auto

---
+++ TeamCode/src/main/java/com/hfrobots/tnt/season2627/FieldCoordinates.java
@@ -0,0 +1,28 @@
+package com.hfrobots.tnt.season2627;
+
+import org.opencv.core.Point;
+
+public class FieldCoordinates {
+    // Flowers
+    public final static Point FLOWER_RED_ALLIANCE_AREA = new Point(0, 48);
+
+    public final static Point FLOWER_BLUE_ALLIANCE_AREA = new Point(144,96);
+
+    public final static Point FLOWER_TOP_OF_FIELD = new Point(48,144);
+
+    public final static Point FLOWER_BOTTOM_OF_FIELD = new Point(96,0);
+
+    public final static Point RED_HIVE_CENTER_LINE = new Point (60, 0);
+
+    public final static Point BLUE_HIVE_CENTER_LINE = new Point (84, 0);
+
+    public final static Point GARDEN_BLUE = new Point (132, 144);
+
+    public final static Point GARDEN_RED = new Point (12, 24);
+
+    public final static Point LOADING_ZONE_BLUE = new Point (144, 36);
+
+    public final static Point LOADING_ZONE_RED = new Point (0, 108);
+
+
+}
